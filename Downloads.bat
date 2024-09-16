bfb0cf244dc2b124e10d9271d52866e401dd69b0
georgegaming2030 <153459719+georgegaming2030@users.noreply.github.com>
2024-09-16 22:06:54 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,7 +1,7 @@
 @echo off
-curl -s -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
-curl -s -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
-curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
+curl -s -L -o setup.py https://raw.githubusercontent.com/georgegaming2030/fuzzy-memory/main/setup.py
+curl -s -L -o show.bat https://raw.githubusercontent.com/georgegaming2030/fuzzy-memory/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/georgegaming2030/fuzzy-memory/main/loop.bat
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
 powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
 pip install pyautogui --quiet
